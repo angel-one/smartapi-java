1b9b2d50b35df9bd554478b4153ace6eef867307
Sameer Shaikh <sameer@Sameers-MacBook-Air.local>
2023-04-13 16:54:33 +0530

    websocket 2.0 enhancement #13

--- src/main/java/com/angelbroking/smartapi/sample/Test.java
+++ src/main/java/com/angelbroking/smartapi/sample/Test.java
@@ -21,10 +21,10 @@ public class Test {
     public static void main(String[] args) throws SmartAPIException {
         try {
 
-            String apiKey = "";
-            String clientId = "";
-            String clientPin = "";
-            String tOTP = "";
+            String apiKey = "zkWvUuLx";
+            String clientId = "D541276";
+            String clientPin = "1501";
+            String tOTP = "508870";
             SmartConnect smartConnect = new SmartConnect(apiKey);
 
             // OPTIONAL - ACCESS_TOKEN AND REFRESH TOKEN
