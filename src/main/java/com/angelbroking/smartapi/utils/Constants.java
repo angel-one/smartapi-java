6d4c4dc578fd1513bdf66755b8bd97bdfab47477
acc-sameer <sameer.shaikh@acc.ltd>
2023-05-30 10:27:53 +0530

    SMARTSTREAM_WSURI prod url changed

--- src/main/java/com/angelbroking/smartapi/utils/Constants.java
+++ src/main/java/com/angelbroking/smartapi/utils/Constants.java
@@ -6,61 +6,44 @@ package com.angelbroking.smartapi.utils;
  */
 public class Constants {
 
-    // Private constructor to prevent instantiation from outside the class
-    private Constants() {
-        // This constructor is intentionally left blank
-    }
-
     /**
      * Product types.
      */
     public static final String PRODUCT_INTRADAY = "INTRADAY";
-
-
     /**
      * Order types.
      */
     public static final String ORDER_TYPE_STOPLOSS_LIMIT = "STOPLOSS_LIMIT";
-
     /**
      * Variety types.
      */
     public static final String VARIETY_STOPLOSS = "STOPLOSS";
-
     /**
      * Transaction types.
      */
     public static final String TRANSACTION_TYPE_BUY = "BUY";
-
     /**
      * Duration types.
      */
     public static final String DURATION_DAY = "DAY";
-
     /**
      * Exchanges.
      */
     public static final String EXCHANGE_NSE = "NSE";
-
-
     /**
      * SmartStream Routes
      */
     public static final String ROOT_URL = "https://apiconnect.angelbroking.com";
     public static final String LOGIN_URL = "https://apiconnect.angelbroking.com/rest/auth/angelbroking/user/v1/loginByPassword";
     public static final String WSURI = "wss://wsfeeds.angelbroking.com/NestHtml5Mobile/socket/stream";
-//    public static final String SMARTSTREAM_WSURI = "ws://smartapisocket.angelone.in/smart-stream";
-    public static final String SMARTSTREAM_WSURI = "ws://mds-uat.angelone.in/smart-stream";
+    public static final String SMARTSTREAM_WSURI = "ws://smartapisocket.angelone.in/smart-stream";
     public static final String SWSURI = "wss://smartapisocket.angelbroking.com/websocket";
-
     /**
      * User Model Constants
      */
     public static final String USER_DATA = "data";
     public static final String USER_PRODUCTS = "products";
     public static final String USER_EXCHANGES = "exchanges";
-
-
     /**
      * Smart WebSocket Constants
      */
@@ -69,7 +52,6 @@ public class Constants {
     public static final String JWT_TOKEN = "jwttoken";
     public static final String CLIENT_CODE = "clientcode";
     public static final String API_KEY = "apikey";
-
     /**
      * SmartAPITicker Constants
      */
@@ -78,7 +60,6 @@ public class Constants {
     public static final String SMART_API_TICKER_TOKEN = "token";
     public static final String SMART_API_TICKER_USER = "user";
     public static final String SMART_API_TICKER_ACCTID = "acctid";
-
     /**
      * SmartStreamTicker Constants
      */
@@ -87,9 +68,6 @@ public class Constants {
     public static final String FEED_TOKEN_HEADER = "x-feed-token";
     public static final String CLIENT_LIB_HEADER = "x-client-lib";
     public static final String CLIENT_LIB_HEADER_VALUE = "JAVA";
-
-
-
     /**
      * SmartConnect Routes Constants
      */
@@ -110,7 +88,6 @@ public class Constants {
     public static final String SMART_CONNECT_API_GTT_LIST = "api.gtt.list";
     public static final String SMART_CONNECT_API_CANDLE_DATA = "api.candle.data";
     public static final String SMART_CONNECT_API_USER_LOGOUT = "api.user.logout";
-
     /**
      * Order model Constants
      */
@@ -128,15 +105,12 @@ public class Constants {
     public static final String TRIGGER_PRICE = "triggerprice";
     public static final String VARIETY = "variety";
     public static final String ORDER_ID = "orderid";
-
     /**
      * ruleList Constants
      */
     public static final String STATUS = "status";
     public static final String PAGE = "page";
     public static final String COUNT = "count";
-
-
     /**
      * SmartConnect Constants
      */
@@ -152,22 +126,17 @@ public class Constants {
     public static final String NULL_URL_MESSAGE = "url cannot be null";
     public static final String INVALID_URL = "Invalid URL: ";
     public static final String API_REQUEST_FAILED_MSG = "Failed to create API request";
-
     public static final String SMART_API_EXCEPTION_ERROR_MSG = "Failed to perform the operation due to an SmartAPIException error.";
     public static final String IO_EXCEPTION_ERROR_MSG = "Failed to perform the operation due to an IO error.";
     public static final String JSON_EXCEPTION_ERROR_MSG = "Failed to perform the operation due to a JSON error.";
-
-
     public static final String ID = "id";
     public static final String QTY = "qty";
     public static final String DISCLOSED_QTY = "disclosedqty";
     public static final String TIME_PERIOD = "timeperiod";
     public static final String ACCEPT = "accept";
-
     public static final String USER_AGENT = "User-Agent";
     public static final String SMART_API_VERSION = "X-Smart API-Version";
     public static final String AUTHORIZATION = "Authorization";
-
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String CLIENT_LOCAL_IP = "X-ClientLocalIP";
     public static final String CLIENT_PUBLIC_IP = "X-ClientPublicIP";
@@ -179,26 +148,23 @@ public class Constants {
     public static final String X_USER_TYPE = "X-UserType";
     public static final String X_SOURCE_ID = "X-SourceID";
     public static final String MESSAGE = "message";
-
     public static final String SMART_API_EXCEPTION_OCCURRED = "SmartAPIException occurred: ";
     public static final String IO_EXCEPTION_OCCURRED = "IOException occurred: ";
     public static final String JSON_EXCEPTION_OCCURRED = "JSONException occurred: ";
     public static final String TICKER_NOT_CONNECTED = "ticker is not connected";
     public static final String TICKER_NOT_NULL_CONNECTED = "ticker is not connected";
-
-
     public static final String SYMBOL_SBINEQ = "SBIN-EQ";
     public static final String MARGIN = "MARGIN";
-
-
     public static final String PARAM_MODE = "mode";
     public static final String PARAM_TOKEN_LIST = "tokenList";
     public static final String PARAM_ACTION = "action";
     public static final String PARAM_PARAMS = "params";
-
     public static final String SMARTAPIREQUESTHANDLER_USER_AGENT = "javasmartapiconnect/3.0.0";
     public static boolean ENABLE_LOGGING = false;
-
+    // Private constructor to prevent instantiation from outside the class
+    private Constants() {
+        // This constructor is intentionally left blank
+    }
 
 
 }
