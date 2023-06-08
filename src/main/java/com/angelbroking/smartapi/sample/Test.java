0201d8358ed8f24c44914f20381127909621fc50
acc-sameer <sameer.shaikh@acc.ltd>
2023-06-08 10:39:19 +0530

    changes in sample test class

--- src/main/java/com/angelbroking/smartapi/sample/Test.java
+++ src/main/java/com/angelbroking/smartapi/sample/Test.java
@@ -2,6 +2,7 @@ package com.angelbroking.smartapi.sample;
 
 import com.angelbroking.smartapi.SmartConnect;
 import com.angelbroking.smartapi.http.exceptions.SmartAPIException;
+import com.angelbroking.smartapi.http.response.HttpResponse;
 import com.angelbroking.smartapi.models.User;
 import com.angelbroking.smartapi.smartstream.SmartStreamListenerImpl;
 import com.angelbroking.smartapi.smartstream.models.ExchangeType;
@@ -22,11 +23,11 @@ public class Test {
 
     public static void main(String[] args) throws SmartAPIException {
         try {
-            String apiKey = "zkWvUuLx";
-            String clientId = "D541276";
-            String clientPin = "1507";
+            String apiKey = "";
+            String clientId = "";
+            String clientPin = "";
             GoogleAuthenticator gAuth = new GoogleAuthenticator();
-            String totp_key = "L6FMTTCWRVSK2PW6AF7A2YMO6Q";
+            String totp_key = "";
             String tOTP = String.valueOf(gAuth.getTotpPassword(totp_key));
             Proxy proxy = Proxy.NO_PROXY;
             SmartConnect smartConnect = new SmartConnect(apiKey,proxy,TIME_OUT_IN_MILLIS);
@@ -47,54 +48,54 @@ public class Test {
             Examples examples = new Examples();
             log.info("getProfile");
             examples.getProfile(smartConnect);
-//
-//            log.info("placeOrder");
-//            HttpResponse placeOrder = examples.placeOrder(smartConnect);
-//
-//
-//            log.info("modifyOrder");
-//            examples.modifyOrder(smartConnect,"230531000603615");
-//
-//            log.info("cancelOrder");
-//            examples.cancelOrder(smartConnect,"230531000603615");
-//
-//            log.info("getOrder");
-//            examples.getOrder(smartConnect);
-//
-//            log.info("getLTP");
-//            examples.getLTP(smartConnect);
-//
-//            log.info("getTrades");
-//            examples.getTrades(smartConnect);
-//
-//            log.info("getRMS");
-//            examples.getRMS(smartConnect);
-//
-//            log.info("getHolding");
-//            examples.getHolding(smartConnect);
-//
-//            log.info("getPosition");
-//            examples.getPosition(smartConnect);
-//
-////            log.info("convertPosition");
-////            examples.convertPosition(smartConnect);
-//
-//            log.info("createRule");
-//            HttpResponse createRuleID = examples.createRule(smartConnect);
-//
-//            log.info("ModifyRule");
-//
-//			log.info("cancelRule");
-//			examples.cancelRule(smartConnect,"865598");
-//
-//            log.info("Rule Details");
-//            examples.ruleDetails(smartConnect, "865598");
-//
-//            log.info("Rule List");
-//            examples.ruleList(smartConnect);
-//
-//            log.info("Historic candle Data");
-//            examples.getCandleData(smartConnect);
+
+            log.info("placeOrder");
+            HttpResponse placeOrder = examples.placeOrder(smartConnect);
+
+
+            log.info("modifyOrder");
+            examples.modifyOrder(smartConnect,"230531000603615");
+
+            log.info("cancelOrder");
+            examples.cancelOrder(smartConnect,"230531000603615");
+
+            log.info("getOrder");
+            examples.getOrder(smartConnect);
+
+            log.info("getLTP");
+            examples.getLTP(smartConnect);
+
+            log.info("getTrades");
+            examples.getTrades(smartConnect);
+
+            log.info("getRMS");
+            examples.getRMS(smartConnect);
+
+            log.info("getHolding");
+            examples.getHolding(smartConnect);
+
+            log.info("getPosition");
+            examples.getPosition(smartConnect);
+
+//            log.info("convertPosition");
+//            examples.convertPosition(smartConnect);
+
+            log.info("createRule");
+            HttpResponse createRuleID = examples.createRule(smartConnect);
+
+            log.info("ModifyRule");
+
+			log.info("cancelRule");
+			examples.cancelRule(smartConnect,"865598");
+
+            log.info("Rule Details");
+            examples.ruleDetails(smartConnect, "865598");
+
+            log.info("Rule List");
+            examples.ruleList(smartConnect);
+
+            log.info("Historic candle Data");
+            examples.getCandleData(smartConnect);
 
             log.info("logout");
             examples.logout(smartConnect);
